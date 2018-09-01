f07aad5edaafe8fc8bb0cb5269f70e24571c3984
Ivan Tanasijevic <ivantanasi111@gmail.com>
2018-09-01 18:28:12 +0200

    settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'catalog.apps.CatalogConfig',
             ],
         },
     },
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belgrade'
 
 USE_I18N = True
 
