d0d4765d6828e15de62b52025aa71625a2fa150f
unknown <ivantanasi111@gmail.com>
2018-09-03 20:42:25 +0200

    part 6

--- catalog/views.py
+++ catalog/views.py
@@ -27,3 +27,20 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+from django.views import generic
+
+
+class BookListView(generic.ListView):
+    model = Book
+    # your own name for the list as a template variable
+    context_object_name = 'my_book_list'
+    queryset = Book.objects.filter(title__icontains='war')[
+        :5]  # Get 5 books containing the title war
+    # Specify your own template name/location
+    template_name = 'books/my_arbitrary_template_name_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
